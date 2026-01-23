d6bb5c42ff79ecd596ece8f95d1d70158b2b2529
RuhamaMalik <malikruhama7@gmail.com>
2026-01-23 19:03:30 +0500

    start js , intro + browser tools, alert , variable etc

---
+++ js/class-01/index.js
@@ -0,0 +1,59 @@
+// one / single line comment
+
+    /*
+    Multi
+    Line
+    Comment
+    */
+
+// console.log("Print From index.js");
+
+/////////////////// 1 - Alerts
+
+// alert('mjhy maza araha hai!')
+
+// console.log("Hum 'Java'Script'' padh rahy hen!");
+// console.log('Mai aik "str"ing"" hun');
+
+
+/////////////////// Variables for Strings (var,let,const)
+
+///// Case-sensitive  apple !== appLe
+//// ; semicolon ---- statement termenatojr
+////  = ---- assignment operator
+
+
+// var x; // Declearation
+// x = 5; // assign value ---- initialization
+// console.log(x); // 5
+
+
+// var username = "Ruhama";
+// console.log(username);
+
+// var age = 10;
+// console.log(age); // 10
+// age = 15; // reassign
+// console.log(age); // 15
+
+// var message = "Hello";
+// alert(message);
+
+// var x = 5;
+// console.log(x);
+// var x = "abcd";
+// console.log(x);
+
+// let a = 5;
+// let a // errror
+
+// const b = 5;
+// const b; // error
+
+var x;
+console.log(x); // undefined
+
+let a;
+console.log(a); // undefined 
+
+// const m; //const' declarations must be initialized
