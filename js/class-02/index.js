201386d41b5858baf625f07c98c4d217459cb8ef
RuhamaMalik <malikruhama7@gmail.com>
2026-01-26 19:14:11 +0500

    variable name legal,illegal, js naming convention, familiar operators, data types

---
+++ js/class-02/index.js
@@ -0,0 +1,121 @@
+///////////////// Data Types
+
+////// Primitive -- direct values --- single value
+
+// string
+// number 
+// boolean  true / false
+// undefined
+// null
+
+
+////// Non Primitive ---- reference --- multiple values
+
+// array
+// objects
+
+
+/////////////// 3 - Variables for Numbers
+
+// let a = 5;
+// let y = 5
+// console.log(a + y); // 10
+
+
+
+///////////// 4-Variable Names Legal and Illegal
+
+//////////   aA-zZ, $, _, num5ber6 
+
+// var myVar;
+// var $myVar;
+// var my$Var$;
+// var _myVar;
+// var myVar1;
+// var my2ndVar;
+// var myPrompt
+
+// var 1stVar;
+// var myVar#;
+// var my Var;
+
+
+
+///////////////////// JS Naming Convention
+
+// var camelCase;
+// var studentName;
+// var saylaniWelfareTrust;
+
+// var PascalCase;
+// var SaylaniWelfareTrust;
+
+// var snack_case;
+// var saylani_welfare_trust;
+
+/////////////// ; statement terminator
+
+
+// var a ; console.log(a); var y =5
+// var a; var b; var c
+
+////////////////// Familiar operators (+ , - , * , / , **, % )
+
+// console.log(5 + 5); // 10
+// console.log(10 - 5); // 5
+// console.log(10 * 5); // 50
+// console.log(10 / 5); // 2
+// console.log(1 / 2); // 0.5
+// console.log(2 / 3); // 0.6666
+// console.log(5 ** 3); // 125
+
+// console.log(2 % 2); // 0
+// console.log(3 % 2); // 1
+// console.log(15 % 4); // 3
+
+
+// var price = 7;
+// var total = price + 5;
+// console.log(total); // 12
+
+///////////////// 6 - Unfamiliar operators (++ , --)
+
+// ++ increament
+// pre increament ++num --- post increament num++
+// -- decreament
+// pre decreament --num --- post decreament num--
+
+
+// var num = 1;
+// num = num + 1;
+// console.log(num); // 2
+
+
+// var num = 1;
+// console.log(num); // 1
+// ++num;
+// console.log(num); // 2
+
+// var num = 1;
+// console.log(num); // 1
+// num++;
+// console.log(num); // 2
+
+
+// var num = 1; // 5
+
+// var exp1 = num++ - ++num + ++num + num++;
+// //          1    -   3   +   4   +  4
+// //            6
+
+
+// console.log(num); // 5
+// console.log(exp1); // 6
+
+// var num = 1; // 6
+// var exp2 = num++ + ++num + num++ - num++ + 13 - num++;
+// //          1    +   3   +   3   -  4    + 13 - 5
+
+
+// console.log(num);  // 6
+// console.log(exp2);  // 11
