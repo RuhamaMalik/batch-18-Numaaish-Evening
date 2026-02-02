26144adb85c1c373aa1d26ad0c50d48fc189fcd9
RuhamaMalik <malikruhama7@gmail.com>
2026-02-02 19:05:35 +0500

    if, else if, nested if else, logical operators

---
+++ js/class-05/index.js
@@ -0,0 +1,104 @@
+// var isRain = true;
+
+// if (isRain) {
+//   console.log("Samosyyy");
+// }else{
+//   console.log("Juice");
+// }
+
+// console.log(5 % 2 === 0);
+
+////////////////// even / odd
+
+// var num = +prompt("Enter Number");
+
+// if(num % 2 === 0){
+// console.log("Even Number");
+// }else{
+//   console.log("Odd Number");
+// }
+
+
+////////////// if, else if
+
+// let num = +prompt("Enter Number");
+
+// if (num > 10) {
+//   console.log("Greater than 10");
+// } else if (num < 10) {
+//   console.log("Less than 10");
+// }else if(num === 10){
+// console.log("Equals to 10");
+// }else{
+//   console.log("Invalid Number");
+// }
+
+/////////// nestef if statement
+
+// const email = "abc@gmail.com";
+// const pswd = "123456";
+
+// let userEmail = prompt("Enter your Email..");
+// let userPswd = prompt("Enter your Pswd");
+
+// if (userEmail === email) {
+//   if (userPswd === pswd) {
+//         alert("User signIn successfully!")
+//   }else{
+//     console.log("Invalid Password");
+//       }
+// } else {
+//   console.log("Account not found!");
+// }
+
+
+// 80 A+
+// 70 A
+// 60 B
+// 50 C
+// Fail
+
+
+// var perc = +prompt("Type your Percentage!");
+
+// if ((perc <= 100) && (perc >= 0)) {
+//   if (perc >= 80) {
+//     alert("MashAllah you got A+");
+//   } else if (perc >= 70) {
+//     alert("MashAllah you got A");
+//   } else if (perc >= 60) {
+//     alert("Alhamdulillah you got B");
+//   } else if (perc >= 50) {
+//     alert("Alhamdulillah you got C");
+//   } else {
+//     alert("Fail!");
+//   }
+
+// } else {
+//   console.log("Invalid Input");
+// }
+
+////////////// 13 -  Testing sets of conditions
+// Logical Operators && , ||
+// && , and operator (aur)
+// || , Or operator (ya)
+
+
+// console.log(true && true); // true
+// console.log(true && false); // false
+// console.log(true && "false"); // "false"
+// console.log(false && "false"); // false
+// console.log(1 && "false" && undefined); // undefined
+// console.log(undefined && "false" && undefined); // undefined pehla wala
+
+// console.log("abcd" && "ayesha"); // ayesha 
+
+
+// console.log(true || undefined || 1); // true
+// // console.log(null || "maryam"); // maryam
+// console.log(null || undefined); // undefined
+
+
+// null && true || false 
+
+console.log(null && true || false );
