9d0d646d3098b677f3f0bb843f0b87333e40f0af
RuhamaMalik <malikruhama7@gmail.com>
2026-02-04 19:04:29 +0500

    if else , array start

---
+++ js/class-06/index.js
@@ -0,0 +1,96 @@
+// console.log(null && true || false && "abcd"); // abcd null false 
+
+
+//////////////////////
+
+
+// let userEmail = prompt("Enter email");
+// let userPswd = prompt("Enter password");
+// let email = "abcd@gmail.com";
+// let pswd = "123456";
+
+// if (userEmail === email) {
+//   if (userPswd === pswd) {
+//     alert("User signIn Successfully")
+//   }else{
+//     console.log("Invalid password!");
+
+//   }
+// } else {
+//   console.log("Invalid Email");
+
+// }
+
+
+
+// if ((userEmail === email) && (userPswd === pswd)) {
+//   alert("User Login Successfully!")
+// }else{
+//   console.log("Invalid Credentialss");
+
+// }
+
+//////////////// a e i o u
+
+// var alpha = prompt("Enter a character").toLowerCase();
+
+// if (alpha === "a"  || alpha === "e" || alpha === "i" || alpha === "o" || alpha === "u") {
+//   console.log("Vowel");
+// } else {
+//   console.log("Consonant");
+// }
+
+
+// document.writeln(`
+//    <h1>Hello class</h1>
+//   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure itaque aperiam quia, porro sunt et saepe omnis
+//     asperiores autem illo deleniti reiciendis iusto unde nemo laboriosam voluptatem eos, minima ducimus!</p>
+
+//   `)
+
+///////////// Weather Card Assignment
+
+/////////////////////// 15 - Arrays
+
+// let user1 = "zainab"
+// let user2 = "misbah"
+// let user3 = "zoha";
+
+// let abcd = new Array("zainab", "misbah","zoha");
+// console.log(abcd);
+
+
+// let users = ["zainab", "misbah","zoha"];
+// console.log(users);
+
+// let arr = ["string", 677, true, ["a","b","c"] , {} ];
+
+
+// let fruits = ["strawberry", "blue berry", "mango", "grapes", "watermelon"];
+
+// length --- no of Elements --- start 1 ---- 5
+// index ---  position of an element --- start 0 --- 0 - 4
+// console.log(fruits);
+// console.log(fruits.length); // 5
+
+// arrayName[index]
+
+// console.log(fruits[2]); // mango
+// console.log(fruits[1]);
+
+// console.log(fruits[5]); // undefined
+
+// fruits[1] = "orange";
+// console.log(fruits);
+
+// fruits[2] = "papaya";
+// console.log(fruits);
+
+
+// fruits[5] = "banana";
+// console.log(fruits);
+
+
+// fruits[8] = "orange";
+// console.log(fruits);  // length : 9 
+
