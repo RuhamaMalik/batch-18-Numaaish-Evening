e5ffc816a5080f76f18bf3d3108d96765ba21f42
RuhamaMalik <malikruhama7@gmail.com>
2026-02-13 19:06:37 +0500

    loops complete

---
+++ js/class-10/index.js
@@ -0,0 +1,55 @@
+// const cities = ["peshawar", "islamabad", "abottabad", "quetta", "lahore"];
+
+// let arr = [["a", "b", "c"], [1, 2, 3,4,5], ["x", "y", "z"]]
+
+// //           3
+// for (let i = 0; i < arr.length; i++) {
+//   // console.log(arr[i]);
+
+//   //          0
+//   for (let j = 0; j < arr[i].length; j++) {
+//     console.log(arr[i][j]); // arr[2][0] 3
+//   }
+
+// }
+
+
+////////////////////////////// print counting
+
+// for (let i = 1; i <= 100; i += 10) {
+//   // document.writeln(i + "<br>");
+
+//   for (let j = i; j < i + 10; j++) {
+//     document.writeln(j + " ");
+//   }
+
+// document.writeln("<br>")
+// 
+
+
+// for (let i= 100;i>=1;i-=10){
+//   // document.writeln(i)
+//   for (let j = i;j>i-10;j--){
+//     document.writeln(j + " ")
+//   }document.writeln(`<br>`)
+// }
+
+
+// let fName = ["zumar", "kanzul", "ayesha", "misbah", "maheen", "ramla"];
+
+// let lName = ["yousuf", "haya", "naz", "noor", "kanwal", "bilal"];
+
+// let fullName = [];
+// // console.log(fName[0] + " " + lName[0]);
+
+// for (let i = 0; i < fName.length; i++) {
+//   // console.log(fName[i]);
+//   fullName.push(fName[i] + " " + lName[i])
+// }
+
+
+// console.log(fullName);
+
+
+
+
