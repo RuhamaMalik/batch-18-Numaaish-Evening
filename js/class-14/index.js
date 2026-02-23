98a4f5725020b8ab703a26786e47519884fa1822
RuhamaMalik <malikruhama7@gmail.com>
2026-02-23 13:22:24 +0500

    string and number convertion

---
+++ js/class-14/index.js
@@ -0,0 +1,68 @@
+// let str = "5.99 15";
+
+// console.log(typeof str);
+
+/////////// parseInt, parseFloat
+
+// str = parseInt(str);
+// console.log(str); // 5 number
+// console.log(typeof str);
+
+// console.log(parseInt("20 30")); // 20
+// console.log(parseInt("10 years")); // 10
+// console.log(parseInt("years 10")); // NaN
+// console.log(parseInt("78")); // 78
+// console.log(parseInt("78.34767")); // 78
+// console.log(parseInt("1"+1)); //  11
+
+
+// console.log(parseInt("1"+1 , 2)); // 3
+// console.log(parseInt("10" , 8)); // 8
+
+
+// console.log(parseFloat("5.99"));
+// console.log(parseFloat("10.55 years"));// 10.55
+
+// let total = "100.79"; // 100.79
+
+// console.log(Number(total));
+// console.log(Number("10 years")); // NaN
+
+// var num = 45;
+// console.log(num.toString() + 5); // 455 string
+
+// let isLoggedIn = true;
+// console.log(isLoggedIn.toString().slice(2));
+
+// console.log(undefined.toString());
+// console.log(undefined + "");
+// console.log(45 + "");
+
+// console.log(100.45463.toFixed(3)); // 100.455
+// console.log(100.45463.toFixed()); // 100 string
+// console.log(55.2.toFixed(2));
+
+
+// let num = 55;
+// console.log(num.toFixed(2)); // 55.00
+// console.log(num.toFixed()); // 55 string
+
+///////////////////// 31 - Getting the current date and time
+
+
+// let date = new Date();
+// date = date.toString();
+// console.log(date.slice(4, 10));
+// console.log(typeof date);
+// console.log(date);
+
+// console.log(typeof null);
+
+
+
+
+
+
+
+
+
