1a10fc7d131071bd519f873e81b885ecb0160bc2
RuhamaMalik <malikruhama7@gmail.com>
2026-02-27 13:16:41 +0500

    function start

---
+++ js/class-16/index.js
@@ -0,0 +1,65 @@
+// let date = new Date();
+// date.setFullYear(2027);
+// date.setMonth(7);
+// date.setDate(15);
+// console.log(date);
+
+// console.log(date);
+
+// console.log(date);
+// console.log(date.toTimeString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+
+
+///////////// Functions
+
+/////// declare
+
+// console.log("Before Function");
+
+// function functionName (){
+//   console.log("Hello from function");
+// }
+
+
+// console.log("After Function");
+
+//////// invoc/call
+
+// functionName();
+// functionName();
+// functionName();
+// functionName();
+// functionName();
+
+
+// function sum (parameters){
+//   console.log(10 + 5);
+// }
+
+// sum(argument);
+// sum();
+// sum();
+// sum();
+
+////////////////////////////
+
+// function sum(num1,num2) {
+//   console.log(num1 + num2);
+// }
+
+
+// sum(5, 3); // 8
+// sum(10,4); // 14
+// sum(15,5); // 20
+
+////////////////////////////
+
+// function add(a, b) {
+//   console.log(a); // 5
+//   console.log(b); // undefined
+// }
+
+// add(5);
+
