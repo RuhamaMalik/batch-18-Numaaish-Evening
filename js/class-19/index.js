8f5434dc1812e03486b519889f6726c027083d4f
RuhamaMalik <malikruhama7@gmail.com>
2026-03-06 13:16:00 +0500

    read input values

---
+++ js/class-19/index.js
@@ -0,0 +1,31 @@
+
+// document.getElementById("form").addEventListener("submit", function () {
+//   console.log("hello");
+// });
+
+///////////////////////////////
+
+
+// function printName(x) {
+//   x.preventDefault();
+//   let myInput = document.getElementById("name");
+//   if(myInput.value){
+//     console.log(myInput.value);
+//   }else{
+//      console.log(myInput.value);
+//     console.log("name is required!");
+//   }
+//   myInput.value = "";
+// }
+
+// document.getElementById("form").addEventListener("submit", printName);
+
+/////////////////////////////// Setting field values
+
+
+// function fillCity(){
+//   let zipCode = document.getElementById("zip").value;
+//   console.log(zipCode);
+// }
+
+// document.getElementById("zip").addEventListener("blur", fillCity)
