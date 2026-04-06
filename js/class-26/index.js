cf259a20454a34a769a911e3e30a11d83ee4f6ae
RuhamaMalik <malikruhama7@gmail.com>
2026-04-06 19:00:12 +0500

    window properties

---
+++ js/class-26/index.js
@@ -0,0 +1,44 @@
+// let user = {
+//   name: 'Saba',
+//   email: 'saba@gmail.com',
+//   contact: '+923767878',
+//   isActive: false,
+//   gender: 'Female',
+//   id: 1,
+//   age: 10
+// }
+
+// console.log('gender' in user);
+// console.log(user);
+// Object.prototype.abcd = 'xyz';
+
+// console.log('abcd' in user); // true
+// console.log(user.hasOwnProperty('abcd')); // false
+// console.log(user.hasOwnProperty('age')); // 
+
+
+
+// for (let prop in user) {
+//   console.log(`key : ${prop} --- value : ${user[prop]}`);
+// }
+
+
+
+// let arr = ['Apple', 'Banana', 'orange',{quantity:5}];
+
+// for (const ele of arr) {
+//   console.log(ele);
+// }
+
+
+
+///////////////// 76
+
+// console.log(window.location.href); // current file url
+// console.log(window.location.hostname);
+// console.log( window.location.pathname);
+// console.log( window.location.hash);
+
+
+
+
