b87db1dab424f5e350379de846c9df92b19fc0b0
RuhamaMalik <malikruhama7@gmail.com>
2026-04-10 19:04:16 +0500

    exeptions ,,, revision ,,, book complete

---
+++ js/class-28/index.js
@@ -0,0 +1,40 @@
+// console.log('Before');
+
+// try {
+//   alrt('hello');
+//   throw new Error('masla alert mai hai');
+// } catch (e) {
+//   console.log(e.message);
+// }finally{
+//   console.log('Mai dono k saath hun!');
+// }
+
+// console.log('After');
+
+
+// document.getElementById('btn').addEventListener('click',function(){
+//   console.log('Success');
+// })
+
+// function myFunc(e){
+//   console.log('Success');
+// }
+
+
+// document.getElementById('btn').addEventListener('click',myFunc)
+
+
+
+// document.getElementById('form').addEventListener('submit', function (event) {
+
+//   event.preventDefault();
+//   console.log(event);
+
+//   let name = document.getElementById('name');
+//   console.log(name.value);
+// })
+
+let str = 'cat';
+
+console.log(str[2]);
+
