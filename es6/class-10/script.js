1cf335d9dae3dedde8b76757f050160f83e13402
RuhamaMalik <malikruhama7@gmail.com>
2026-05-08 18:58:32 +0500

    arry methods complete+optional chaning

---
+++ es6/class-10/script.js
@@ -0,0 +1,72 @@
+// Array.map((item)=>{
+// return something
+// })
+// let num=[1,3,5]
+// let result=num.map(num=>num+8)
+// console.log(result);
+
+
+// let num=[11,4,55]
+// let result=num.map((num)=>{
+//   return num *2
+// })
+// console.log(result);
+
+
+//includes
+// let fruit=["Apple","MANGO","BANANA"]
+// let result=fruit.includes("guava")
+// console.log(result);
+
+//join()
+
+// let fruit=["Apple","MANGO","BANANA"]
+// let result=fruit.join("")
+// console.log(result);
+
+
+
+// const words=["I","love","Javascript"]
+// console.log(words.join(" "));
+
+
+//reduce//
+
+// let nums=[1,2,3,4,5]
+// let total=nums.reduce((acc,nums)=>{
+//   return acc+nums
+// })
+// console.log(total);
+
+// let name="ruhama gull malik"
+// let result=name.split("").reverse().join()
+// console.log(result);
+
+
+
+// let word="bisma"
+// let result=word.split("").reverse().join("")
+// if(word==result){
+//   console.log("its true");
+  
+// }else{
+//   console.log("its false");
+  
+// }
+
+
+const user={
+  add:{
+    city:"karachi"
+  }
+};
+console.log(user.address?.city);
+
+
+
+
+
+
+
+
+
